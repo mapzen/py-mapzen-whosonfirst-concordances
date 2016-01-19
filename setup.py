335a6d3804269af5b5cf5a697df7c90389441f3d
Dan Phiffer <dan@phiffer.org>
2016-01-19 15:33:38 -0800

    🔧 Added VERSION file

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
+# Remove .egg-info directory if it exists, to avoid dependency problems with
+# partially-installed packages (20160119/dphiffer)
+
+import os, sys
+from shutil import rmtree
+
+cwd = os.path.dirname(os.path.realpath(sys.argv[0]))
+egg_info = cwd + "/mapzen.whosonfirst.concordances.egg-info"
+if os.path.exists(egg_info):
+    rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
-desc = open("README.md").read(),
+desc = open("README.md").read()
+version = open("VERSION").read()
 
 setup(
     name='mapzen.whosonfirst.concordances',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.concordances'],
-    version='0.06',
+    version=version,
     description='Tools for working with Who\'s On First concordances',
     author='Mapzen',
-    url='https://github.com/mapzen/py-mapzen-whosonfirst-concordances',
+    url='https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances',
     install_requires=[
         ],
     dependency_links=[
@@ -21,5 +33,5 @@ setup(
         'scripts/wof-dump-concordances',
         'scripts/wof-index-concordances',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-concordances/releases/tag/v0.06',
+    download_url='https://github.com/whosonfirst/py-mapzen-whosonfirst-concordances/releases/tag/' + version,
     license='BSD')
